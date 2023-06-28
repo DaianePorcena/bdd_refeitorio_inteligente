5a5b22d72add96db78c6889b15a35236f22a7194
DaianePorcena <daianeporcena7@gmail.com>
2023-06-28 00:14:56 -0300

    .

--- steps/liberar_aluno.py
+++ steps/liberar_aluno.py
@@ -1,9 +1,12 @@
-from behave import when, then 
+from behave import when, then
 from refeitorio import *
+
 
 @when("a probabilidade de liberacao for {probabilidade} porcento")
 def when_probabilidade_de_liberacao(context, probabilidade):
-    context. total_de_alunos_liberados = liberar_alunos(context.alunos_reconhecidos, int(probabilidade))
+    context. total_de_alunos_liberados = liberar_alunos(
+        context.alunos_reconhecidos, int(probabilidade))
+
 
 @then("pelo menos, um(a) aluno deve ser liberado")
 def then_alunos_liberados(context):
